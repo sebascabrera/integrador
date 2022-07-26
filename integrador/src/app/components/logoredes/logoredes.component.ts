37cac03ea9664aa5bc8e8dc7c4bb1d12eb48b47f
Sebastian <sebasfedele@gmail.com>
2022-07-26 15:24:45 -0300

    Logo de redes sociales

---
+++ integrador/src/app/components/logoredes/logoredes.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-logoredes',
+  templateUrl: './logoredes.component.html',
+  styleUrls: ['./logoredes.component.css']
+})
+export class LogoredesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
