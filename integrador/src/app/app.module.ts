e56bf0ce47778ce3b0db7a56bc792bfe88c75fcf
Sebastian <sebasfedele@gmail.com>
2022-07-26 16:20:50 -0300

    Banner y bootstrap cargado

--- integrador/src/app/app.module.ts
+++ integrador/src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { LogoargprogramaComponent } from './components/logoargprograma/logoargprograma.component';
 import { LogoredesComponent } from './components/logoredes/logoredes.component';
+import { BannerComponent } from './components/banner/banner.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     LogoargprogramaComponent,
-    LogoredesComponent
+    LogoredesComponent,
+    BannerComponent
   ],
   imports: [
     BrowserModule,
