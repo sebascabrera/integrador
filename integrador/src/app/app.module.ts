b18869b4370d7a9334d943870c13be4461e33f71
Sebastian <sebasfedele@gmail.com>
2022-07-27 13:01:21 -0300

    imagen acerca de mi

--- integrador/src/app/app.module.ts
+++ integrador/src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { LogoargprogramaComponent } from './components/logoargprograma/logoargprograma.component';
 import { LogoredesComponent } from './components/logoredes/logoredes.component';
+import { AcercaDeComponent } from './components/acerca-de/acerca-de.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     LogoargprogramaComponent,
-    LogoredesComponent
+    LogoredesComponent,
+    AcercaDeComponent
   ],
   imports: [
     BrowserModule,
